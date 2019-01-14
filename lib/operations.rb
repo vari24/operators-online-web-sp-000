baaf40d0e8a0456930bd0aeaa374e4fa7243e8cc
Sara Varisara <varisara.n@gmail.com>
2019-01-14 15:32:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+	if speed < 40
+		return true
+	elsif speed > 60
+		return true
+	elsif 40 < speed && 60 > speed
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 && speed < 40 ? true : false
 end
-	
-
-
